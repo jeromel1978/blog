e3286bde8b6e752ceba2f86f6ed8a1e34d59f62a
jeromel1978 <jeromelosorata@hotmail.com>
2022-12-02 12:03:10 -0600

    Initial

---
+++ src/components/Index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Login from "./Login.svelte";
+  import Main from "./Main.svelte";
+  let LoggedIn = false;
+  const ConfirmLogIn = () => {
+    LoggedIn = true;
+  };
+</script>
+
+{#if LoggedIn}
+  <Main />
+{:else}
+  <Login on:loggedin={ConfirmLogIn} />
+{/if}
---
+++ src/components/Login.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  let userid: string;
+  let password: string;
+
+  const dispatch = createEventDispatcher();
+  const Login = () => {
+    dispatch("loggedin");
+  };
+  $: CantLogin = !userid && !password;
+</script>
+
+<div>
+  <input type="text" bind:value={userid} placeholder="User ID" />
+  <input type="password" bind:value={password} placeholder="Password" />
+  <button on:click={Login} disabled={CantLogin}>Login</button>
+</div>
